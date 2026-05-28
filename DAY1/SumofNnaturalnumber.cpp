1794560f4a186cbd6bc4d8dd1fa61407d878bb27
shlokmaurya124-tech <shlokmaurya124@gmail.com>
2026-05-28 17:07:57 +0530

    Add files via upload

---
+++ DAY1/SumofNnaturalnumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, sum = 0;
+    cout << "Enter the value of n :";
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    cout << " The sum of first " << n << " natural number is : " << sum;
+    return 0;
+}
