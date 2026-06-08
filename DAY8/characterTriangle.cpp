7a7e4c70fcae981cc75fff045694f86faad2927d
shlokmaurya124-tech <shlokmaurya124@gmail.com>
2026-06-08 21:08:45 +0530

    Add files via upload

---
+++ DAY8/characterTriangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the height of the triangle pattern : ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        char ch = 'A';
+        for (char j = 0; j <= i; j++)
+        {
+            cout << ch;
+            ch += 1;
+        }
+        cout << endl;
+    }
+    return 0;
+}
