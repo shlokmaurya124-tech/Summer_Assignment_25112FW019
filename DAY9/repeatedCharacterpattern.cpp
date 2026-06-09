a013d0d26528ec9e7a9ecce225e589b293aab415
shlokmaurya124-tech <shlokmaurya124@gmail.com>
2026-06-09 14:34:04 +0530

    Add files via upload

---
+++ DAY9/repeatedCharacterpattern.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the height of character pattern : ";
+    cin >> n;
+    char ch = 'A';
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << char(65+i);
+            ch++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
